c8a07732a2c2ce34ada986242edb7f5c2f93e1ee
sofiauldemiaclear <sofy.torresv@gmail.com>
2024-03-06 00:25:52 -0500

    Add:functionality to the project card for the effect of rotating and showing description

--- src/Components/cardProject/CardProject.jsx
+++ src/Components/cardProject/CardProject.jsx
@@ -1,12 +1,59 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import "./cardProject.css";
 
 const CardProject = ({ project }) => {
+    const { name, liveLink, githubLink, description, skills } = project;
+
+    const [isFlipped, setFlipped] = useState(false);
+
+    const handleFlip = () => {
+        console.log("click en ver detalles");
+        setFlipped(!isFlipped);
+    };
+
+    const handleBack = () => {
+        // Volver al card-front
+        setFlipped(false);
+    };
+    console.log("estado actual de la tarjeta");
     return (
-        <section className="card-project">
-            <div className="img-project">
-                <img src={project.image} alt="" />
+        <section className={`card-project ${isFlipped ? "flipped" : ""}`}>
+            <div className="card-front">
+                <img src={project.image} alt={name} />
+                <h2 className="subtitle-projects">{name}</h2>
+                <button>
+                    <a href={liveLink} target="blank" rel="noopener noreferrer">
+                        Codigo
+                    </a>
+                </button>
+                <button>
+                    <a
+                        href={githubLink}
+                        target="blank"
+                        rel="noopener noreferrer"
+                    >
+                        Codigo
+                    </a>
+                </button>
+
+                <button className="details-button" onClick={handleFlip}>
+                    Ver Detalles
+                </button>
+
+                <h2>{skills}</h2>
             </div>
+
+            {isFlipped && (
+                <div className="card-back">
+                    <div className="card-back-content">
+                        {description}
+                        <button className="back-button" onClick={handleBack}>
+                            Volver
+                        </button>
+                    </div>
+                </div>
+            )}
         </section>
     );
 };
@@ -18,6 +65,7 @@ CardProject.propTypes = {
         date: PropTypes.string.isRequired,
         image: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
+        skills: PropTypes.string.isRequired,
         liveLink: PropTypes.string.isRequired,
         githubLink: PropTypes.string.isRequired,
     }).isRequired,
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,10 +1,46 @@
+import './Contact.css'
+
 const Contact = () => {
     return (
         <section id="contact">
-               <section>
-                    <h1>Hola, Soy </h1>
+                <section className='container-main-home'>
+            <div className='box'>
 
-                </section>
+              <div className='container-hello'>
+                <span className='span-hello'>Hola. mi nombre es</span>
+                <h1 className='title-home'>Sofia Torres</h1>
+                <span className='span-frontend'>  &gt; Frontend Developer</span>
+               
+                <p className='text-home'> Apasionada por el diseño visual y fascinada por el css.</p>
+                <div className="about__image-social">
+                <p className='text-home'>Si buscas talento comprometido, hablemos</p>
+                            <a href="#" className="about__social-link">
+                                <i className="bx bxs-envelope"></i>
+                            </a>
+                            <a href="#" className="about__social-link">
+                                <i className="bx bxl-linkedin"></i>
+                            </a>
+                            <a href="#" className="about__social-link">
+                                <i className="bx bxl-github"></i>
+                            </a>
+                        </div>
+              </div>
+
+              <div className='container-github-profile'>
+                {/* <p className='titleGithubProfile'>//Find my profile on Github:</p> */}
+                <div className='containerGithubLink'>
+                  <p>
+                    {/* <span className='githubLinkCode'>const </span> */}
+                    {/* <span className='githubLinkVariable'>githubLink </span> */}
+                    {/* <span className='githubLinkOperator'>= </span> */}
+                    {/* <a className='githubLinkURL' href='https://github.com/andrenavas' target='_blank'>'https://github.com/andrenavas'</a> */}
+                    {/* <span className='githubLinkOperator'>;</span> */}
+                  </p>
+                </div>
+              </div>
+            </div>
+            {/* <Button linkTo='./projects' descriptionButton='view-projects'/> */}
+          </section>
         </section>
     );
 };
