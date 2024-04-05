e60734bf43529cfa18ded2a7943c35e9c1808985
sofiauldemiaclear <sofy.torresv@gmail.com>
2024-04-05 18:00:04 -0500

    Update:enlaces de imágenes de la carpeta data y agregando funciones map y filter en el archivo home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import "./Home.css";
-import imagen2 from "../../assets/images/react-portfolio.png";
-import imagen1 from "../../assets/images/js.png";
-import imagen3 from "../../assets/images/materialui-porfolio.png";
-import imagen5 from "../../assets/images/git-fork-outline.png";
-import imagen4 from "../../assets/images/wordpress-about.png";
+import data from "../../mocks/ProjectData";
+// import imagen2 from "../../assets/images/react-portfolio.png";
+// import imagen1 from "../../assets/images/js.png";
+// import imagen3 from "../../assets/images/materialui-porfolio.png";
+// import imagen5 from "../../assets/images/git-fork-outline.png";
+// import imagen4 from "../../assets/images/wordpress-about.png";
 
 const Home = () => {
+    const { aboutData } = data;
+       // Filtrar las habilidades deseadas
+       const filteredSkills = aboutData.skillsWithImages.filter(skill =>
+        ['React', 'JavaScript', 'Material UI', 'Git', 'Wordpress'].includes(skill.name)
+    );
     return (
         <section className="home-view">
             <div className="box">
@@ -14,55 +20,23 @@ const Home = () => {
                     <h1 className="title-home">Sofia Torres</h1>
                     <span className="home__text">
                         Desarrolladora Frontend, apasionada por el diseño visual
-                        y fascinada por el CSS. Conóce mis principales habilidades.
+                        y fascinada por el CSS. Conóce mis principales
+                        habilidades.
                     </span>
                 </div>
             </div>
-            
-                
-                <div className="intro__images">
-                    <div className="content__images">
+
+            <div className="intro__images">
+                {filteredSkills.map((skill, index) => (
+                    <div className="content__images" key={index}>
                         <img
-                            src={imagen1}
-                            alt="Descripción de la imagen 1"
+                            src={skill.image}
+                            alt={`Imagen de ${skill.name}`}
                             className="intro__image"
                         />
-                        <p className="content__text">JavaScript</p>
+                        <p className="content__text">{skill.name}</p>
                     </div>
-                    <div className="content__images">
-                        <img
-                            src={imagen2}
-                            alt="Descripción de la imagen 2"
-                            className="intro__image"
-                        />
-                        <p className="content__text" >React</p>
-                    </div>
-                    <div className="content__images">
-                        <img
-                            src={imagen3}
-                            alt="Descripción de la imagen 2"
-                            className="intro__image"
-                        />
-                        <p className="content__text" >Material UI</p>
-                    </div>
-                    <div className="content__images">
-                    <img
-                            src={imagen5}
-                            alt="Descripción de la imagen 2"
-                            className="intro__image"
-                        /> 
-                        <p className="content__text" >Git</p>
-            
-                    </div>
-                    <div className="content__images">
-                    <img
-                            src={imagen4}
-                            alt="Descripción de la imagen 2"
-                            className="intro__image"
-                        />
-                        <p className="content__text" >Wordpress</p>
-               
-                    </div>
+                ))}
             </div>
         </section>
     );
